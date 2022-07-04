3064c9e94afd9f9993923581430db6b999e56a5c
panayo <papoutsispanayotis@gmail.com>
2022-07-04 11:41:47 +0300

    update gitignore

---
+++ README.md
@@ -0,0 +1,4 @@
+# mlops-zoomcamp
+Repo containing my codes for the DataTalksClub MLOps zoocamp course (https://github.com/DataTalksClub/mlops-zoomcamp).
+
+<img src="./images/banner.png" alt="MLOps Robot"/>
---
+++ week4-deployment/web-service/test.py
@@ -0,0 +1,17 @@
+# import predict 
+import requests
+
+
+ride = {
+    "PULocationID": 10,
+    "DOLocationID": 50,
+    "trip_distance": 40
+}
+
+url = 'http://localhost:9696/predict'
+response = requests.post(url, json=ride)
+print(response.json())
+
+# features = predict.prepare_features(ride)
+# pred = predict.predict(features)
+# print(pred)
